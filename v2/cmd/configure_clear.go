1851b2c6493b92b65f44d321893f70a5425047c2
jthabet <jihad.thabet@gmail.com>
2020-09-17 15:25:33 +0200

    updated clearCmd to iterate and deletes every keyring entry corresponding to a config.

--- v2/cmd/configure_clear.go
+++ v2/cmd/configure_clear.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 
@@ -14,29 +12,39 @@ import (
 )
 
 var noKeyringDefined bool
+var forceClear bool
 
 var clearCmd = &cobra.Command{
 	Use:   "clear",
 	Short: "Clear current configuration",
 	Long:  "Clear current authentication data from your local keyring",
 	Run: func(cmd *cobra.Command, args []string) {
-		filePath := rest.DefaultConfigFilePath()
-		if s, err := ioutil.ReadFile(filePath); err == nil {
-			var c rest.CecConfig
-			if err = json.Unmarshal(s, &c); err == nil {
-				if !noKeyringDefined {
-					// First clean the keyring
-					if err := rest.ClearKeyring(&c); err == nil {
-						fmt.Println(promptui.IconGood + " Removed tokens from keychain")
-					} else {
-						fmt.Println(promptui.IconBad + " Error while removing token from keyring: " + err.Error())
-					}
-				}
-			}
-		}
-		if err := os.Remove(filePath); err != nil {
+
+		list, err := rest.GetConfigList()
+		if err != nil {
 			log.Fatal(err)
 		}
+
+		if !forceClear {
+			pConfirm := promptui.Prompt{Label: "You are going to clear everything", IsConfirm: true, Default: "Y"}
+			confirm, err := pConfirm.Run()
+			if err != nil || confirm == "N" {
+				fmt.Println("Nothing has been removed")
+				return
+			}
+		}
+
+		for _, v := range list.Configs {
+			if err := rest.ClearKeyring(v); err != nil {
+				log.Printf("%s\n", err)
+			}
+		}
+		fmt.Println(promptui.IconGood + " Successfully cleared the keyring")
+
+		if err := os.Remove(rest.GetConfigFilePath()); err != nil {
+			log.Fatal(err)
+		}
+
 		fmt.Println(promptui.IconGood + " Successfully removed config file")
 	},
 }
@@ -45,5 +53,6 @@ func init() {
 	flags := clearCmd.PersistentFlags()
 	helpMsg := "Explicitly tell the tool to *NOT* try to use a keyring. Only use this flag if you really know what your are doing: some sensitive information will end up stored on your file system in clear text."
 	flags.BoolVar(&noKeyringDefined, "no-keyring", false, helpMsg)
+	flags.BoolVarP(&forceClear, "force", "f", false, "Force deletes without confirmation")
 	RootCmd.AddCommand(clearCmd)
 }
